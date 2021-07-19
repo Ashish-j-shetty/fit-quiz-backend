4945f3c82e03bea4c068ed77e0f17385507bbcd2
AJ <ashishshetty2612@gmail.com>
2021-07-19 15:11:32 +0530

    feat: data for quiz

--- src/routes/quiz.router.js
+++ src/routes/quiz.router.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
+const quizData = require("../db/data");
+
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-  res.json({ success: true, message: "quiz data fetched successfully" });
+  res.json({
+    success: true,
+    quiz: quizData,
+    message: "quiz data fetched successfully",
+  });
 });
 
 module.exports = router;

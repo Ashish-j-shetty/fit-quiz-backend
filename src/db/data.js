ba8c148a6a6cae77f87d912a792f92e14bb761d8
AJ <ashishshetty2612@gmail.com>
2021-07-19 15:19:28 +0530

    refactor : quiz ds

--- src/db/data.js
+++ src/db/data.js
@@ -1,277 +1,274 @@
-const quizData = {
-  allquize: [
-    {
-      image:
-        "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_e94c98c0-4579-11e7-a744-0b8c60cf3127.jpg",
-      name: "Quiz on fitnes?",
-      id: "quiz-1",
-      questions: [
-        {
-          id: "q_FvNgCz12b",
-          negativePoints: 5,
-          points: 10,
-          question: "Does muscle weigh more than fat?",
-          img: "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_23251950-457a-11e7-a744-0b8c60cf3127.jpg",
-          options: [
-            {
-              text: "yes",
-              id: "a_X6A6IqsbEu",
-              isAnwer: false,
-            },
-            {
-              text: "No",
-              id: "a_LwalLKu9L4",
-              isAnwer: true,
-            },
-          ],
-        },
-        {
-          id: "q_UeSUPKozU-",
-          negativePoints: 5,
-          points: 10,
-          question:
-            "Maintaining physical fitness requires major lifestyle changes.",
-          img: "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_49408020-457a-11e7-a744-0b8c60cf3127.jpg",
+const quizData = [
+  {
+    image:
+      "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_e94c98c0-4579-11e7-a744-0b8c60cf3127.jpg",
+    name: "Quiz on fitnes?",
+    id: "quiz-1",
+    questions: [
+      {
+        id: "q_FvNgCz12b",
+        negativePoints: 5,
+        points: 10,
+        question: "Does muscle weigh more than fat?",
+        img: "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_23251950-457a-11e7-a744-0b8c60cf3127.jpg",
+        options: [
+          {
+            text: "yes",
+            id: "a_X6A6IqsbEu",
+            isAnwer: false,
+          },
+          {
+            text: "No",
+            id: "a_LwalLKu9L4",
+            isAnwer: true,
+          },
+        ],
+      },
+      {
+        id: "q_UeSUPKozU-",
+        negativePoints: 5,
+        points: 10,
+        question:
+          "Maintaining physical fitness requires major lifestyle changes.",
+        img: "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_49408020-457a-11e7-a744-0b8c60cf3127.jpg",
 
-          options: [
-            {
-              text: "True",
-              id: "a_XKILcVh0hx",
-              isAnwer: false,
-            },
-            {
-              text: "False",
-              id: "a_qaC9baS2s6",
-              isAnwer: true,
-            },
-          ],
-        },
-        {
-          id: "q_7NmX5GPi1o",
-          negativePoints: 5,
-          points: 10,
-          question: "No pain, no gain is a workout myth.",
-          img: "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_5f8a8650-457a-11e7-a744-0b8c60cf3127.jpg",
+        options: [
+          {
+            text: "True",
+            id: "a_XKILcVh0hx",
+            isAnwer: false,
+          },
+          {
+            text: "False",
+            id: "a_qaC9baS2s6",
+            isAnwer: true,
+          },
+        ],
+      },
+      {
+        id: "q_7NmX5GPi1o",
+        negativePoints: 5,
+        points: 10,
+        question: "No pain, no gain is a workout myth.",
+        img: "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_5f8a8650-457a-11e7-a744-0b8c60cf3127.jpg",
 
-          options: [
-            {
-              text: "True",
-              id: "a_M279Xp1HR1",
-              isAnwer: true,
-            },
-            {
-              text: "False",
-              id: "a_80kG-Vku-C",
-              isAnwer: false,
-            },
-          ],
-        },
-        {
-          id: "q_1dq9qDIZmC",
-          negativePoints: 5,
-          points: 10,
-          question:
-            "Weight loss should be the most important goal of fitness and exercise.",
-          img: "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_8150eae0-457a-11e7-a744-0b8c60cf3127.jpg",
+        options: [
+          {
+            text: "True",
+            id: "a_M279Xp1HR1",
+            isAnwer: true,
+          },
+          {
+            text: "False",
+            id: "a_80kG-Vku-C",
+            isAnwer: false,
+          },
+        ],
+      },
+      {
+        id: "q_1dq9qDIZmC",
+        negativePoints: 5,
+        points: 10,
+        question:
+          "Weight loss should be the most important goal of fitness and exercise.",
+        img: "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_8150eae0-457a-11e7-a744-0b8c60cf3127.jpg",
 
-          options: [
-            {
-              text: "True",
-              id: "a_nqHezvD4v7",
-              isAnwer: false,
-            },
-            {
-              text: "False",
-              id: "a_gj90Qj9Xnv",
-              isAnwer: true,
-            },
-          ],
-        },
-        {
-          id: "q_zeXDcBr5CX",
-          negativePoints: 5,
-          points: 10,
-          question: "You should warm up before exercising.",
-          img: "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_8150eae0-457a-11e7-a744-0b8c60cf3127.jpg",
+        options: [
+          {
+            text: "True",
+            id: "a_nqHezvD4v7",
+            isAnwer: false,
+          },
+          {
+            text: "False",
+            id: "a_gj90Qj9Xnv",
+            isAnwer: true,
+          },
+        ],
+      },
+      {
+        id: "q_zeXDcBr5CX",
+        negativePoints: 5,
+        points: 10,
+        question: "You should warm up before exercising.",
+        img: "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/591cf05d4691d600116701e3/_8150eae0-457a-11e7-a744-0b8c60cf3127.jpg",
 
-          options: [
-            {
-              text: "True",
-              id: "a_DMi8yc7lQ9",
-              isAnwer: true,
-            },
-            {
-              text: "False",
-              id: "a_NJ4uytA6tK",
-              isAnwer: false,
-            },
-          ],
-        },
-      ],
-    },
-    {
-      image:
-        "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/5bcbc4c79fea260013e6cb72/_d795c510-d4c6-11e8-bb1d-cb291eba3229.jpg",
-      name: "Nutrition & Healthy Eating ?",
-      id: "quiz-2",
-      questions: [
-        {
-          id: "q_FvNgCz12b-1",
+        options: [
+          {
+            text: "True",
+            id: "a_DMi8yc7lQ9",
+            isAnwer: true,
+          },
+          {
+            text: "False",
+            id: "a_NJ4uytA6tK",
+            isAnwer: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    image:
+      "https://s3-us-west-1.amazonaws.com/tryinteract-uploads/5bcbc4c79fea260013e6cb72/_d795c510-d4c6-11e8-bb1d-cb291eba3229.jpg",
+    name: "Nutrition & Healthy Eating ?",
+    id: "quiz-2",
+    questions: [
+      {
+        id: "q_FvNgCz12b-1",
 
-          negativePoints: 5,
-          points: 10,
-          question: "Which of these is a common source of vitamin A?",
-          img: "https://www.highspeedtraining.co.uk/hub/wp-content/uploads/2018/07/potatoes_vitamin_a_nutrition-7.jpg",
-          options: [
-            {
-              text: "Cheese",
-              id: "a_X6A6IqsbEu",
-              isAnwer: false,
-            },
-            {
-              text: "Potatoes especially with the skin",
-              id: "a_LwalLKu9L4",
-              isAnwer: false,
-            },
-            {
-              text: "Fruits and vegetables particularly carrots",
-              id: "a_X6A6IqsbEu1",
-              isAnwer: true,
-            },
-            {
-              text: "Brewer's yeast",
-              id: "a_X6A6IqsbEu2",
-              isAnwer: false,
-            },
-          ],
-        },
-        {
-          id: "q_UeSUPKozU-13",
-          negativePoints: 5,
-          points: 10,
-          question:
-            "What is the primary purpose of making sure your body receives carbohydrates?",
-          img: "https://www.highspeedtraining.co.uk/hub/wp-content/uploads/2018/07/carbohydrates-7.jpg",
+        negativePoints: 5,
+        points: 10,
+        question: "Which of these is a common source of vitamin A?",
+        img: "https://www.highspeedtraining.co.uk/hub/wp-content/uploads/2018/07/potatoes_vitamin_a_nutrition-7.jpg",
+        options: [
+          {
+            text: "Cheese",
+            id: "a_X6A6IqsbEu",
+            isAnwer: false,
+          },
+          {
+            text: "Potatoes especially with the skin",
+            id: "a_LwalLKu9L4",
+            isAnwer: false,
+          },
+          {
+            text: "Fruits and vegetables particularly carrots",
+            id: "a_X6A6IqsbEu1",
+            isAnwer: true,
+          },
+          {
+            text: "Brewer's yeast",
+            id: "a_X6A6IqsbEu2",
+            isAnwer: false,
+          },
+        ],
+      },
+      {
+        id: "q_UeSUPKozU-13",
+        negativePoints: 5,
+        points: 10,
+        question:
+          "What is the primary purpose of making sure your body receives carbohydrates?",
+        img: "https://www.highspeedtraining.co.uk/hub/wp-content/uploads/2018/07/carbohydrates-7.jpg",
 
-          options: [
-            {
-              text: "To minimise the body's ability to absorb calcium",
-              id: "a_X6A16IqsbEu",
-              isAnwer: false,
-            },
-            {
-              text: "To heighten cholesterol levels",
-              id: "a_LwadlLKu9L4",
-              isAnwer: false,
-            },
-            {
-              text: "To improve bone marrow",
-              id: "a_eX6A6IqsbEu1",
-              isAnwer: false,
-            },
-            {
-              text: "To provide the body with energy",
-              id: "a_X6A6IqsbEud2",
-              isAnwer: true,
-            },
-          ],
-        },
-        {
-          id: "q_7NmX5GPi1o4",
-          negativePoints: 5,
-          points: 10,
-          question:
-            "What ill-effect might be caused by the over-consumption of fluids?",
-          img: "https://www.highspeedtraining.co.uk/hub/wp-content/uploads/2018/07/drinks_overconsumption_consequences-15.jpg",
+        options: [
+          {
+            text: "To minimise the body's ability to absorb calcium",
+            id: "a_X6A16IqsbEu",
+            isAnwer: false,
+          },
+          {
+            text: "To heighten cholesterol levels",
+            id: "a_LwadlLKu9L4",
+            isAnwer: false,
+          },
+          {
+            text: "To improve bone marrow",
+            id: "a_eX6A6IqsbEu1",
+            isAnwer: false,
+          },
+          {
+            text: "To provide the body with energy",
+            id: "a_X6A6IqsbEud2",
+            isAnwer: true,
+          },
+        ],
+      },
+      {
+        id: "q_7NmX5GPi1o4",
+        negativePoints: 5,
+        points: 10,
+        question:
+          "What ill-effect might be caused by the over-consumption of fluids?",
+        img: "https://www.highspeedtraining.co.uk/hub/wp-content/uploads/2018/07/drinks_overconsumption_consequences-15.jpg",
 
-          options: [
-            {
-              text: "A weakened immune system",
-              id: "a_X6A16IqsbEu4",
-              isAnwer: false,
-            },
-            {
-              text: "Dilution and lowering of sodium levels in the blood",
-              id: "a_LwadlLKu9L44",
-              isAnwer: true,
-            },
-            {
-              text: "Increased conversion to fat",
-              id: "a_eX6A6IqsbEu14",
-              isAnwer: false,
-            },
-            {
-              text: "Aggravated stomach acids",
-              id: "a_X6A6IqsbEud24",
-              isAnwer: false,
-            },
-          ],
-        },
-        {
-          id: "q_1dq9qDIZmC5",
-          negativePoints: 5,
-          points: 10,
-          question:
-            "How many grams of fibre is it recommended we eat every day?",
-          img: "https://www.highspeedtraining.co.uk/hub/wp-content/uploads/2018/07/carrots_fibre_daily_intake-7.jpg",
+        options: [
+          {
+            text: "A weakened immune system",
+            id: "a_X6A16IqsbEu4",
+            isAnwer: false,
+          },
+          {
+            text: "Dilution and lowering of sodium levels in the blood",
+            id: "a_LwadlLKu9L44",
+            isAnwer: true,
+          },
+          {
+            text: "Increased conversion to fat",
+            id: "a_eX6A6IqsbEu14",
+            isAnwer: false,
+          },
+          {
+            text: "Aggravated stomach acids",
+            id: "a_X6A6IqsbEud24",
+            isAnwer: false,
+          },
+        ],
+      },
+      {
+        id: "q_1dq9qDIZmC5",
+        negativePoints: 5,
+        points: 10,
+        question: "How many grams of fibre is it recommended we eat every day?",
+        img: "https://www.highspeedtraining.co.uk/hub/wp-content/uploads/2018/07/carrots_fibre_daily_intake-7.jpg",
 
-          options: [
-            {
-              text: "200",
-              id: "a_X6A16IqsbEu5",
-              isAnwer: false,
-            },
-            {
-              text: "At least 18",
-              id: "a_LwadlLKu9L45",
-              isAnwer: true,
-            },
-            {
-              text: "No more than 50",
-              id: "a_eX6A6IqsbEu15",
-              isAnwer: false,
-            },
-            {
-              text: "5-10",
-              id: "a_X6A6IqsbEud25",
-              isAnwer: false,
-            },
-          ],
-        },
-        {
-          id: "q_zeXDcBr5CX7",
-          negativePoints: 5,
-          points: 10,
-          question:
-            "Which nutrients are especially important for promoting good bone strength?.",
-          img: "https://www.highspeedtraining.co.uk/hub/wp-content/uploads/2018/07/milk_calcium-7.jpg",
+        options: [
+          {
+            text: "200",
+            id: "a_X6A16IqsbEu5",
+            isAnwer: false,
+          },
+          {
+            text: "At least 18",
+            id: "a_LwadlLKu9L45",
+            isAnwer: true,
+          },
+          {
+            text: "No more than 50",
+            id: "a_eX6A6IqsbEu15",
+            isAnwer: false,
+          },
+          {
+            text: "5-10",
+            id: "a_X6A6IqsbEud25",
+            isAnwer: false,
+          },
+        ],
+      },
+      {
+        id: "q_zeXDcBr5CX7",
+        negativePoints: 5,
+        points: 10,
+        question:
+          "Which nutrients are especially important for promoting good bone strength?.",
+        img: "https://www.highspeedtraining.co.uk/hub/wp-content/uploads/2018/07/milk_calcium-7.jpg",
 
-          options: [
-            {
-              text: "Carbohydrates",
-              id: "a_X6A16IqsbEu7",
-              isAnwer: false,
-            },
-            {
-              text: "Calcium and vitamin D",
-              id: "a_LwadlLKu9L47",
-              isAnwer: true,
-            },
-            {
-              text: "Omega 3",
-              id: "a_eX6A6IqsbEu17",
-              isAnwer: false,
-            },
-            {
-              text: "Iron and sodium",
-              id: "a_X6A6IqsbEud27",
-              isAnwer: false,
-            },
-          ],
-        },
-      ],
-    },
-  ],
-};
+        options: [
+          {
+            text: "Carbohydrates",
+            id: "a_X6A16IqsbEu7",
+            isAnwer: false,
+          },
+          {
+            text: "Calcium and vitamin D",
+            id: "a_LwadlLKu9L47",
+            isAnwer: true,
+          },
+          {
+            text: "Omega 3",
+            id: "a_eX6A6IqsbEu17",
+            isAnwer: false,
+          },
+          {
+            text: "Iron and sodium",
+            id: "a_X6A6IqsbEud27",
+            isAnwer: false,
+          },
+        ],
+      },
+    ],
+  },
+];
 
 module.exports = quizData;
